fd8384672ce3ad0165ac777afcf7b79504568efd
nhutam03 <nhutam050@gmail.com>
2025-02-08 02:29:34 +0700

    update login ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import LoginForm from "@/components/LoginForm";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from "@/components/ui/command";
-import { Check, MoreHorizontal } from "lucide-react";
+import { Check, AlignJustify } from "lucide-react";
 import { cn } from "@/lib/utils";
 // import { useAuthStore } from "@/stores/authStore";
 //import { useRouter } from "next/navigation";
@@ -38,44 +38,49 @@ export default function LoginPage() {
       <div className="flex flex-col items-center justify-center gap-5">
         <div className="flex flex-col items-center justify-center">
           <Image src="/bondhublogo.png" width={300} height={100} alt="Bondhub Logo" />
-          {/* <img src="/bondhublogo.png" style={{ width: "300px" }} alt="Bondhub Logo" /> */}
           <h2 className="text-center text-l text-gray-600">
             Đăng nhập tài khoản Bondhub để kết nối với ứng dụng Bondhub Web
           </h2>
         </div>
-        <div className="p-7 bg-white shadow-lg rounded-xl flex flex-col items-center gap-4 relative w-[541px] h-[523px]">
-          <p>{showLoginForm ? "Đăng nhập bằng mật khẩu" : "Đăng nhập bằng quét mã QR"}</p>
-          {!showLoginForm ?
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <Button variant="ghost" size="icon" className="absolute top-2 right-2">
-                <MoreHorizontal className="w-5 h-5" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandList>
-                  <CommandEmpty>Không tìm thấy.</CommandEmpty>
-                  <CommandGroup>
-                    <CommandItem
-                      value="password-login"
-                      onSelect={() => handleSelect("password-login")}
-                    >
-                      Đăng nhập bằng mật khẩu
-                      <Check
-                        className={cn(
-                          "ml-auto",
-                          showLoginForm ? "opacity-100" : "opacity-0"
-                        )}
-                      />
-                    </CommandItem>
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover> : null
-          }
-          {showLoginForm ? <LoginForm /> : <QrLogin />}
+        <div className="bg-white shadow-lg rounded-[30px] flex flex-col items-center gap-4 relative w-[541px] h-[523px]">
+          <div className="flex flex-row border-b border-gray-200 w-full h-[60px] justify-center items-center font-semibold">
+            <p>{showLoginForm ? "Đăng nhập bằng mật khẩu" : "Đăng nhập bằng quét mã QR"}</p>
+            {!showLoginForm ?
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <Button variant="ghost" size="icon" className="absolute top-2 right-2 border mr-2 w-[45px] h-[35px]">
+                    <AlignJustify className="w-5 h-5" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[220px] p-0">
+                  <Command>
+                    <CommandList>
+                      <CommandEmpty>Không tìm thấy.</CommandEmpty>
+                      <CommandGroup>
+                        <CommandItem
+                          className="flex items-center cursor-pointer"
+                          value="password-login"
+                          onSelect={() => handleSelect("password-login")}
+                        >
+                          Đăng nhập bằng mật khẩu
+                          <Check
+                            className={cn(
+                              "ml-auto",
+                              showLoginForm ? "opacity-100" : "opacity-0"
+                            )}
+                          />
+                        </CommandItem>
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover> : null
+            }
+          </div>
+          <div>
+            {showLoginForm ? <LoginForm /> : <QrLogin />}
+          </div>
+          
           {showLoginForm ?
           <a className="text-[#39a8f5] font-semibold cursor-pointer" onClick={() => handleSelect('qr-login')}>Đăng nhập bằng mã QR</a>
             : null}
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useAuthStore } from "@/stores/authStore";
 import { useRouter } from "next/navigation";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Smartphone, Lock } from "lucide-react";
 
 export default function LoginForm() {
   const { loginWithPhoneNumber } = useAuthStore();
@@ -24,22 +27,41 @@ export default function LoginForm() {
   };  
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-      <input
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 justify-center items-center">
+      {/* <input
         type="phoneNumber"
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
         placeholder="Phone number"
         required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Mật khẩu"
-        required
-      />
-      <button type="submit">Đăng nhập</button>
+      /> */}
+      <div className="flex items-center gap-2 border-b border-gray-200 mb-3">
+        <Smartphone className="w-5 h-5" />
+        <Input
+          className="w-[350px] h-[50px]"
+          type="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          placeholder="Số điện thoại"
+          required />
+      </div>
+      <div className="flex items-center gap-2 border-b border-gray-200 mb-7">
+        <Lock className="w-5 h-5" />
+        <Input
+          className="w-[350px] h-[50px]"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Mật khẩu"
+          required
+        />
+      </div>
+      
+      <Button
+        className="w-[373px] h-[50px] bg-[#80c7f9] hover:bg-[#0068ff] text-white font-semibold rounded-md mb-3"
+       type="submit">Đăng nhập với mật khẩu</Button>
+
+      <a className="cursor-pointer" >Quên mật khẩu</a>
     </form>
   );
 }
